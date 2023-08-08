d9ae29b958e6f8bafe61a53c85ae7ab9a2940eab
Tosho Joy <omotosojoy@gmail.com>
2023-08-08 23:47:05 +0100

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-modulus = number % 10
+modulus = abs(number) % 10
+if number < 0:
+    modulus = -modulus
 print(f"Last digit of {number:d} is {modulus:d} and", end=" ")
 if modulus > 5:
     print("is greater than 5")
-elif modulus == 0:
+elif modulus in range(1, 6):
     print("is less than 6 and not 0")
 else:
-    print("is less than 6 and not 0")
+    print("is 0")
