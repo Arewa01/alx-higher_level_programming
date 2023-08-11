cbd59ed98dcb321b216b8e838977ff02fdfdeb5c
Tosho Joy <omotosojoy@gmail.com>
2023-08-11 14:21:32 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ arguments = len(argv) - 1
 if arguments == 0:
     print("{} arguments.".format(arguments))
 elif arguments == 1:
-    print("{} argument".format(arguments))
+    print("{} argument:".format(arguments))
 else:
-    print("{} arguments".format(arguments))
+    print("{} arguments:".format(arguments))
 
 if arguments > 0:
     for i, arg in enumerate(argv[1:], start=1):
