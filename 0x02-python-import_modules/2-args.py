5a7e773516991a6c721394433388e6be71b53180
Tosho Joy <omotosojoy@gmail.com>
2023-08-11 14:20:18 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 from sys import argv
 arguments = len(argv) - 1
 
-if arguments == 1:
+if arguments == 0:
+    print("{} arguments.".format(arguments))
+elif arguments == 1:
     print("{} argument".format(arguments))
 else:
     print("{} arguments".format(arguments))
