9cc649633d8edd0778b487aa0e9c29898227609a
Tosho Joy <omotosojoy@gmail.com>
2023-08-16 13:52:30 +0100

    fixes made to Task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,14 @@
 
 if __name__ == "__main__":
     """Prints the number and list of arguments"""
-    from sys import argv  
+    from sys import argv
     arguments = len(argv) - 1
-
+    if arguments > 0:
+        for i in range(arguments):
+            print("{}: {}".format(i + 1, argv[i + 1]))
     if arguments == 0:
         print("{} arguments.".format(arguments))
     elif arguments == 1:
         print("{} argument:".format(arguments))
     else:
         print("{} arguments:".format(arguments))
-    if arguments > 0:
-        for i in range(arguments):
-            print("{}: {}".format(i + 1, argv[i + 1]))
